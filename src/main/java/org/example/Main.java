49f72706a7264379e33444512736fa3a0f12c045
MeryemBor <116949045+MeryemBor@users.noreply.github.com>
2022-11-27 13:05:52 +0000

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,7 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("What a fast week we had");
         //Github ucuncu gun
+        //Github ilk deneme
 
     }
 
